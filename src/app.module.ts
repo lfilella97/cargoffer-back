f484fcb497a33846be70446d62d59d568458a3e4
lfilella97 <lfilella97@gmail.com>
2023-04-12 01:23:18 +0200

    Add database provider and create Get /tasks controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
+import * as dotenv from "dotenv";
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TasksModule } from "./tasks/tasks.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { databaseProviders } from "./database/database.providers";
+
+dotenv.config();
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, MongooseModule.forRoot(`${process.env.DATABASE_URL}`)],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ...databaseProviders],
 })
 export class AppModule {}
--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,24 @@
-export class Task {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+export type TaskDocument = Task & Document;
+
+@Schema()
+export class Task {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  category: string;
+
+  @Prop({ required: true })
+  due_date: Date;
+
+  @Prop({ required: true })
+  _id: string;
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task);
