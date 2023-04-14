ab9a695894721ac88300932e0b512cf273112a32
lfilella97 <lfilella97@gmail.com>
2023-04-14 19:27:40 +0200

    Add filter by querry on get tasks controller and service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,6 +6,7 @@ import {
   Delete,
   Param,
   Patch,
+  Query,
 } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 import { Task } from "./entities/task.entity";
@@ -15,8 +16,8 @@ export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  findAll() {
-    return this.tasksService.findAll();
+  findAll(@Query() query: { category: string }) {
+    return this.tasksService.findAll(query.category);
   }
 
   @Post("create")
